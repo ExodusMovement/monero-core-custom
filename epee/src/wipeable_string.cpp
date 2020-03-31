5047cc97a6a279fde1ce11b90d26829a7dd67ea6
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2020-03-31 15:48:49 +0200

    feat: add Linux support

--- epee/src/wipeable_string.cpp
+++ epee/src/wipeable_string.cpp
@@ -27,6 +27,7 @@
 // THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <boost/optional/optional.hpp>
+#include <memory>
 #include <string.h>
 #include "memwipe.h"
 #include "misc_log_ex.h"
