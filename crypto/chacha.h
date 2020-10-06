a7fd4e4c4710d6ad1002e7d6cd8fc9716f30894e
Сковорода Никита Андреевич <chalkerx@gmail.com>
2020-10-06 03:31:50 +0300

    Restore a static assert

--- crypto/chacha.h
+++ crypto/chacha.h
@@ -33,6 +33,7 @@
 #include <stdint.h>
 #include <stddef.h>
 
+#define CHACHA_KEY_SIZE 32
 #define CHACHA_IV_SIZE 8
 
 #if defined(__cplusplus)
@@ -43,12 +44,16 @@
 #include "hash.h"
 
 namespace crypto {
+  using chacha_key = epee::mlocked<tools::scrubbed_arr<uint8_t, CHACHA_KEY_SIZE>>;
+
 #pragma pack(push, 1)
   // MS VC 2012 doesn't interpret `class chacha_iv` as POD in spite of [9.0.10], so it is a struct
   struct chacha_iv {
     uint8_t data[CHACHA_IV_SIZE];
   };
 #pragma pack(pop)
+
+  static_assert(sizeof(chacha_key) == CHACHA_KEY_SIZE && sizeof(chacha_iv) == CHACHA_IV_SIZE, "Invalid structure size");
 }
 
 #endif
