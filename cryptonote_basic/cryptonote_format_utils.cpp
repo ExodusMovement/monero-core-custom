b000556d9c24d8b30faf50afe362060d2f642a47
andyvo <danh.vo@exodus.io>
2022-08-20 00:50:53 +0700

    fix: check output type

--- cryptonote_basic/cryptonote_format_utils.cpp
+++ cryptonote_basic/cryptonote_format_utils.cpp
@@ -778,29 +778,33 @@ namespace cryptonote
     }
   }
   //---------------------------------------------------------------
-  bool check_output_types(const transaction &tx, const uint8_t hf_version)
+  bool check_output_types(const transaction& tx, const uint8_t hf_version)
   {
-    for (const auto &o : tx.vout)
+    for (const auto &o: tx.vout)
     {
       if (hf_version > HF_VERSION_VIEW_TAGS)
       {
         // from v15, require outputs have view tags
-        CHECK_AND_ASSERT_MES(o.target.type() == typeid(txout_to_tagged_key), false, "wrong variant type: " << o.target.type().name() << ", expected txout_to_tagged_key in transaction id=" << get_transaction_hash(tx));
+        CHECK_AND_ASSERT_MES(o.target.type() == typeid(txout_to_tagged_key), false, "wrong variant type: "
+          << o.target.type().name() << ", expected txout_to_tagged_key in transaction id=" << get_transaction_hash(tx));
       }
       else if (hf_version < HF_VERSION_VIEW_TAGS)
       {
         // require outputs to be of type txout_to_key
-        CHECK_AND_ASSERT_MES(o.target.type() == typeid(txout_to_key), false, "wrong variant type: " << o.target.type().name() << ", expected txout_to_key in transaction id=" << get_transaction_hash(tx));
+        CHECK_AND_ASSERT_MES(o.target.type() == typeid(txout_to_key), false, "wrong variant type: "
+          << o.target.type().name() << ", expected txout_to_key in transaction id=" << get_transaction_hash(tx));
       }
-      else //(hf_version == HF_VERSION_VIEW_TAGS)
+      else  //(hf_version == HF_VERSION_VIEW_TAGS)
       {
         // require outputs be of type txout_to_key OR txout_to_tagged_key
         // to allow grace period before requiring all to be txout_to_tagged_key
-        CHECK_AND_ASSERT_MES(o.target.type() == typeid(txout_to_key) || o.target.type() == typeid(txout_to_tagged_key), false, "wrong variant type: " << o.target.type().name() << ", expected txout_to_key or txout_to_tagged_key in transaction id=" << get_transaction_hash(tx));
+        CHECK_AND_ASSERT_MES(o.target.type() == typeid(txout_to_key) || o.target.type() == typeid(txout_to_tagged_key), false, "wrong variant type: "
+          << o.target.type().name() << ", expected txout_to_key or txout_to_tagged_key in transaction id=" << get_transaction_hash(tx));
 
         // require all outputs in a tx be of the same type
-        CHECK_AND_ASSERT_MES(o.target.type() == tx.vout[0].target.type(), false, "non-matching variant types: " << o.target.type().name() << " and " << tx.vout[0].target.type().name() << ", "
-                                                                                                                << "expected matching variant types in transaction id=" << get_transaction_hash(tx));
+        CHECK_AND_ASSERT_MES(o.target.type() == tx.vout[0].target.type(), false, "non-matching variant types: "
+          << o.target.type().name() << " and " << tx.vout[0].target.type().name() << ", "
+          << "expected matching variant types in transaction id=" << get_transaction_hash(tx));
       }
     }
     return true;
